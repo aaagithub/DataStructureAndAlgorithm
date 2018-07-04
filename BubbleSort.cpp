bd10f93949ea93ee5ffb3360d350c4c1da2c4a7a
Wang <w04051041@163.com>
2018-07-04 11:20:12 +0800

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,11 +1,23 @@
 //冒泡排序
 void BubbleSort(int a[], int n)
 {
-  for(int i = 0; i < n; i ++)
-    for(int j = 0; j < n-i; j++)
+    bool swap = false;
+    for(int i = 0; i < n; i ++)
     {
-       if(a[j] < a[j+1])
-       {
-       }
+        for(int j = 0; j < n-i; j++)
+        {
+            if(a[j] > a[j+1])
+            {
+                int tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = a[j];
+                swap = true;
+            }
+        }
+        //没有发生位置交换，整体有序退出
+        if(false == swap)
+        {
+            return;
+        }
     }
 }
