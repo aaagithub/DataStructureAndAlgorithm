2c7e3a916b3225f8769d7871f7da84e0dc4cd441
aaa <w04051041@163.com>
2018-07-04 16:49:24 +0800

    Update

--- SortAlgorithm.cpp
+++ SortAlgorithm.cpp
@@ -1,4 +1,5 @@
-//冒泡排序
+﻿//冒泡排序
+//最坏：O(n2)     平均：O(n2)     最好：O(n)
 void BubbleSort(int a[], int n)
 {
     bool swap = false;
@@ -21,3 +22,47 @@ void BubbleSort(int a[], int n)
         }
     }
 }
+
+
+//选择排序
+//最坏：O(n2)     平均：O(n2)     最好：O(n2)
+void SelectionSort(int a[], int n)
+{
+	for (int i = 0; i < n-1; i++)
+	{
+		int index = i;
+		for (int j = i + 1; j < n; j++)
+		{
+			//找到无序部分最小元素索引
+			if (a[j] < a[index])
+			{
+				index = j;
+			}
+		}
+		//将最小元素前移
+		int temp = a[i];
+		a[i] = a[index];
+		a[index] = temp;
+	}
+}
+
+
+//插入排序
+//最坏：O(n2)     平均：O(n2)     最好：O(n)
+void InsertionSort(int a[], int n)
+{
+	for (int i = 1; i < n; i++)
+	{
+		int j = i - 1;
+		int temp = a[i];
+		//移出插入位置
+		while (j >= 0 && a[j] > temp)
+		{
+			a[j + 1] = a[j];
+			j--;
+		}
+		a[j + 1] = temp;
+	}
+}
+
+
--- SortAlgorithm.h
+++ SortAlgorithm.h
@@ -1,7 +1,13 @@
-//�����㷨�����n��������������
-//ð������
+﻿//排序算法均针对n个数的整形数组
+//冒泡排序
+//最坏：O(n2)     平均：O(n2)     最好：O(n)
 void BubbleSort(int a[], int n);
 
-//��������
+//选择排序
+//最坏：O(n2)     平均：O(n2)     最好：O(n2)
+void SelectionSort(int a[], int n);
 
+//插入排序
+//最坏：O(n2)     平均：O(n2)     最好：O(n)
+void InsertionSort(int a[], int n);
 
