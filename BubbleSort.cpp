a9fd57a7b157ae40cfbd010671b395ba1a4d43e0
Wang <w04051041@163.com>
2018-07-04 11:35:58 +0800

    Create BubbleSort.cpp
    Bubble sort with swap flag.

---
+++ BubbleSort.cpp
@@ -0,0 +1,23 @@
+//冒泡排序
+void BubbleSort(int a[], int n)
+{
+    bool swap = false;
+    for(int i = 0; i < n; i ++)
+    {
+        for(int j = 1; j < n-i; j++)
+        {
+            if(a[j] > a[j-1])
+            {
+                int tmp = a[j];
+                a[j] = a[j-1];
+                a[j-1] = tmp;
+                swap = true;
+            }
+        }
+        //没有发生位置交换，整体有序退出
+        if(false == swap)
+        {
+            return;
+        }
+    }
+}
