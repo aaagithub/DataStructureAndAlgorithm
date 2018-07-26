f91b2dce27f1d5bbb39918646e870d78e33ad65f
Wang <w04051041@163.com>
2018-07-26 11:09:32 +0800

    Add QuickSort and MergeSort

--- SortAlgorithm.h
+++ SortAlgorithm.h
@@ -11,3 +11,10 @@ void SelectionSort(int a[], int n);
 //最坏：O(n2)     平均：O(n2)     最好：O(n)
 void InsertionSort(int a[], int n);
 
+
+//快速排序
+//l=0  r=n-1
+void QuickSort(int a[],int l, int r);
+
+//归并排序
+void MergeSort(int a[], int n);
